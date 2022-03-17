ba1299fce01cc9214671a84c7a76a6e1839cf50e
Daniel Verduras Gallego <danielverdurasgallego@gmail.com>
2022-03-17 02:47:17 +0100

    jugando y pedir columna

--- conecta4.pl
+++ conecta4.pl
@@ -20,14 +20,23 @@
 
 
 
-jugar:- generar_tablero_inicial(L), escribir_tablero(L), !.
+jugar:- generar_tablero_inicial(L), escribir_tablero(L), jugando('X', L), !.
+
+%%jugando('X', L) :- ganador('O', L), write('Gana jugador 2').
+%%jugando('O', L) :- ganador('X', L), write('Gana jugador 1').
+%%jugando(_, L) :- completo(L), write('Empate').
+jugando('X', L) :- pedir_input(C), write(C), nl, escribir_tablero(L), jugando('O', L).
+jugando('O', L) :- pedir_input(C), write(C), nl, escribir_tablero(L), jugando('X', L).
+
+%% VICTORIAS %%
+
 %%%% código del profe:
 
 lista_repe(1,X,[X]).
 lista_repe(N,X,[X|L]):- N1 is N-1, lista_repe(N1,X,L).
 
 generar_tablero_inicial(L) :- lista_repe(7, ' ', L1),
-                              lista_repe(6,L1,L).
+                              lista_repe(6,L1,L), !.
 
 %%imprime los elementos de una lista
 escribir_lista([]).
@@ -53,12 +62,22 @@ escribir_tablero1([X|L]):- lista_repe(15,'-',L1), write(''),
 
 %% generar_tablero_inicial(L), escribir_tablero(L).
 
-pedir_input(Ficha,TOld):-
+pedir_input(C):-
     write("Introduzca la columna: "),
-    nl, read(Col), 
-    introducir_ficha(Col,Ficha,TOld)
+    nl, read(C1), C is C1 - 1, col(C).
 
-introducir_ficha(Col,Ficha,TOld,TNew):- 
+%columnas validas
+col(0).
+col(1).
+col(2).
+col(3).
+col(4).
+col(5).
+col(6).
+
+
+%%colocar(P, C, L, L1) :-
+%%introducir_ficha(Col,Ficha,TOld,TNew):-
     
     %%%validacion.
 
